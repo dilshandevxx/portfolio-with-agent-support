0080baa2160f3239b4ee5f98c106f0bf6214756b
Dilshan P.A.A.I <dilshaprathapaarachchi@gmail.com>
2025-12-27 12:24:32 +0530

    add maria to sopport customers

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Script src="https://unpkg.com/@elevenlabs/convai-widget-embed" async strategy="afterInteractive" />
+        <elevenlabs-convai agent-id="agent_2701kdf79z7pf708exw3jjmgcpdv"></elevenlabs-convai>
       </body>
     </html>
   );
